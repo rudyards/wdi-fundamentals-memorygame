d9313b7ba55eb2f13341acb39450c7422c7b8b4f
rudyards <rudy@mightycheese.com>
2019-04-13 15:26:39 -0700

    Control Flow/Functions/Objects

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,41 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cards = [
+{
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+},
+{
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+},
+{
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+},
+{
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+}
+];
+var cardsInPlay = [];
+
+var checkForMatch = function(){
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log("You found a match!");
+    } else {
+      console.log("Sorry, try again.");
+    }
+}
+var cardFlip = function(cardID){
+    console.log("User flipped "+cards[cardID].rank+" of "+cards[cardID.suit]);
+    cardsInPlay.push(cards[cardID].rank);
+    console.log(cards[cardID].cardImage)
+    checkForMatch();
+}
+
+cardFlip(0);
+cardFlip(2);
